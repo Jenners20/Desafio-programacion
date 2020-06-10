3fd39193067de2c5782bc71a2c144fcc80fe0d5b
Jenners20 <61439419+Jenners20@users.noreply.github.com>
2020-06-09 23:32:13 -0400

    Add files via upload

---
+++ Ejercicio5/Ejercicio5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Crear un programa que escriba en pantalla los numeros 
+             * pares del 23 al 7 en orden descendente, usando while.*/
+
+            int numeros = 23;
+            double par = 0;
+
+            Console.WriteLine();
+            Console.WriteLine("Los numeros del 23 al 7 en orden descendente son: ");
+            Console.WriteLine();
+
+            while (numeros>7)
+            {
+                numeros = numeros - 1;
+                par = numeros % 2;
+                if (par==0)
+                {
+                    Console.WriteLine(numeros);
+                }
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
